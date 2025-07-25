c29a021c4a0f30b5d557c94a2591882c1291ba19
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-07-25 12:18:12 -0700

    first commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>My Todo list</h1>
+    <div>
+      <h2>Finish React Courses</h2>
+      <button>Delete</button>
+    </div>
+    <div>
+      <h2>Finish ASAP Frontenf</h2>
+      <button>Delete</button>
+    </div>
+    <div>
+      <h2>Land A Junior Job</h2>
+      <button>Delete</button>
+    </div>
+    <div>
+      <h2>Earn $100k+</h2>
+      <button>Delete</button>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Todo.jsx
@@ -0,0 +1,39 @@
+function Todo () {
+    return (
+        <div>Todo Item</div>
+    )
+};
+
+export default Todo;
+
+
+
+
+
+
+
+
+
+
+/**
+function name() {
+    return (
+        <div></div>
+    )
+};
+
+export default name;
+
+*/
+
+/**
+function name() {
+    return (
+        <div></div>
+    )
+};
+
+export dafault name;
+ */
+
+
